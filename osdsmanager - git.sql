f1ec344c3dbfba32f4741f2c9375506cd7f878cc
Daniel <dabuitragoca@gmail.com>
2016-12-13 10:18:27 -0500

    renombrado del archivo osdsmanager
    nuevas queries acerca del "enq: TX - row lock contention"

--- osdsmanager - git.sql
+++ osdsmanager - git.sql
@@ -32,18 +32,14 @@ order by Numero_Sesiones desc;
 --1. consultas que se estan ejecutando 
 select SID, s.SERIAL#, SPID, a.sql_text 
 from v$session s, v$process p, v$sqlarea a 
-where s.paddr=p.addr and s.sql_id=a.sql_id;
-
--- encontrar sessiones de bloqueo 
-SELECT s.blocking_session, s.sid, 
-   s.serial#, s.seconds_in_wait
-FROM v$session s
-WHERE blocking_session IS NOT NULL;
+where s.paddr   = p.addr
+  and s.sql_id  = a.sql_id
+order by 4;
 
 --2. query para encontrar quien bloquea a quien:
 SELECT s1.username || '@' || s1.machine
-|| ' ( SID=' || s1.SID || ' ) is blocking '
-|| s2.username || '@' || s2.machine || ' ( SID=' || s2.SID || ' ) ' AS blocking_status
+|| ' ( SID = ' || s1.SID || ' ) is blocking '
+|| s2.username || '@' || s2.machine || ' ( SID = ' || s2.SID || ' ) ' AS blocking_status
 FROM v$lock l1, v$session s1, v$lock l2, v$session s2
 WHERE s1.SID = l1.SID AND s2.SID = l2.SID
   AND l1.BLOCK = 1 AND l2.request > 0
@@ -51,45 +47,46 @@ WHERE s1.SID = l1.SID AND s2.SID = l2.SID
   AND l1.id2 = l2.id2 
 ORDER BY 1;
 
---3. Conocer el SQL que esta ejecutando un determinado SID
+--3. Conocer la sentencia que esta ejecutando un determinado SID
 SELECT a.sid, a.serial#, b.sql_text
-FROM v$session a,
-  v$sqlarea b
+FROM v$session a, v$sqlarea b
 WHERE a.sql_address = b.address
 AND sid             = 1014;
 
+--4. Kill sessions  'sid,serial#'
+execute sys.kill_osds_session(485, 3554);
 
--- Conocer los parametros de 
+-- la sesion (Waiting_session) esta esperando a que la session (holding_session)
+-- libere algun recurso
+SELECT * FROM dba_waiters;
+
+-- enq: TX - row lock contention
+select w.event,count(9)
+from gv$session s, gv$session_wait w
+where s.sid=w.sid
+group by w.event
+order by 2;
+
+-- identificar usuarios, programas y a q tablas intentan acceder las sessiones
+-- que tienen lock 'enq: TX - row lock contention'
+SELECT object_name,   b.username, b.program, count(9)
+from v$session b, dba_objects o
+where o.object_id=ROW_WAIT_OBJ#
+and event = 'enq: TX - row lock contention'
+group by object_name,  b.username, b.program, ROW_WAIT_FILE#,ROW_WAIT_BLOCK#,ROW_WAIT_ROW#
+order by 4;
+
+-- Conocer los parametros de INI_TRANS y MAX_TRANS
 SELECT table_name, ini_trans, max_trans 
 FROM dba_tables
 WHERE table_name    = 'MSISDN_TARGET'
   AND owner         = 'ACAADMIN';
   
--- * Actualizar INIT y MAX_TRANS
+-- Actualizar INIT y MAX_TRANS
 alter table ACAADMIN.MSISDN_TARGET INITRANS 20 MAXTRANS 255;
 
 -- Parametros de configuracion de la base   
 SELECT * FROM V$NLS_PARAMETERS;
-
--- consultas activas en este momento
- select
-  object_name, 
-  object_type, 
-  session_id, 
-  type,         -- Type or system/user lock
-  lmode,        -- lock mode in which session holds lock
-  request, 
-  block, 
-  ctime         -- Time since current mode was granted
-from
-  v$locked_object, all_objects, v$lock
-where
-  v$locked_object.object_id = all_objects.object_id AND
-  v$lock.id1 = all_objects.object_id AND
-  v$lock.sid = v$locked_object.session_id
-order by
-  session_id, ctime desc, object_name;
-
 
 --give you queries currently running for more than 2 horas
 select s.username,s.sid,s.serial#,s.last_call_et/60 mins_running,q.sql_text from v$session s 
@@ -100,19 +97,10 @@ and type <>'BACKGROUND'
 and last_call_et> 7200
 order by sid,serial#,q.piece;
 
-select inst_id, program, module, SQL_ID, machine
-from gv$session
-where type!='BACKGROUND'
-and status='ACTIVE' 
-and sql_id is not null;
-
 -- =============================================
 
--- verificar el estados de los usarios (LOKED,EXPIRED,OPEN)
+-- verificar el estados de las cuentas de usarios (LOKED,EXPIRED,OPEN)
 select username, account_status from dba_users;
-
-
-SELECT sys_context('USERENV','SID') FROM dual;
 
 select a.session_id,a.oracle_username, a.os_user_name, b.owner "OBJECT OWNER", b.object_name,b.object_type,a.locked_mode from 
 (select object_id, SESSION_ID, ORACLE_USERNAME, OS_USER_NAME, LOCKED_MODE from v$locked_object) a, 
@@ -140,15 +128,6 @@ order      by to_number(p.spid);
 
 column sql_text format a90
 -- a90 -> significa alphanumeric 
-
-SELECT sesion.sid,
-  sql_text
-FROM v$sqltext sqltext,
-  v$session sesion
-WHERE sesion.sql_hash_value = sqltext.hash_value
-AND sesion.sql_address      = sqltext.address
-AND sesion.username        IS NOT NULL
-ORDER BY sqltext.piece;
 
 -- Ver la ruta donde se encuentra el alert.log file
 show parameter BACKGROUND_DUMP_DEST;
